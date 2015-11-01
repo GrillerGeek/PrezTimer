e21230e33dd3c63361d02975ead05f824f991bf0
Jason Robey <jason.robey@gmail.com>
2015-10-31 22:12:31 -0400

    Initial scaffold of app

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('prezTimer', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
