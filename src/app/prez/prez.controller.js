ef94e932f25e91536e9df483643ff9eeb962376f
Jason Robey <jason.robey@gmail.com>
2015-12-27 09:33:21 -0500

    added prez slide buttons

--- src/app/prez/prez.controller.js
+++ src/app/prez/prez.controller.js
@@ -6,16 +6,32 @@
     .controller('PrezController', PrezController);
 
   /** @ngInject */
-  function PrezController(prezService, $log) {
+  function PrezController(prezService, $location, $log) {
     var vm = this;
-
-    vm.presentation = prezService.prez;
+    var presentation = prezService.prez;
+    
+    vm.presentation = presentation;
+    vm.newPresentation = newPresentation;
+    vm.previousSlide = previousSlide;
+    vm.nextSlide = nextSlide;
     
     activate();
 
     function activate() {
         $log.info(vm.presentation);    
     }
+    
+    function newPresentation() {
+        $location.path('/');
+    }
+    
+    function previousSlide() {
+        presentation.currentSlide += -1;
+    }
 
+    function nextSlide() {
+        presentation.currentSlide += 1;
+    }
+    
   }
 })();
