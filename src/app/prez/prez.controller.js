60e7dd8c59ff7c886707a10d91f5ebd2920d2ccd
Jason Robey <jason.robey@gmail.com>
2015-12-29 17:47:52 -0500

    added message when no prez is defined

--- src/app/prez/prez.controller.js
+++ src/app/prez/prez.controller.js
@@ -11,18 +11,21 @@
     var presentation = prezService.prez;
     
     vm.presentation = presentation;
-    vm.newPresentation = newPresentation;
     vm.previousSlide = previousSlide;
     vm.nextSlide = nextSlide;
+    vm.blockTimer = false;
+    vm.message = '';
+    vm.alertType = 'alert-info';
     
     activate();
 
     function activate() {
-        $log.info(vm.presentation);    
-    }
-    
-    function newPresentation() {
-        $location.path('/');
+        $log.info(vm.presentation);
+        if (!vm.presentation.title || vm.presentation.title === '') {
+            vm.blockTimer = true;
+            vm.alertType = 'alert-danger';
+            vm.message = 'You did not define a presentation. Return to home and provide the presenation details';
+        } 
     }
     
     function previousSlide() {
